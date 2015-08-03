444679089f8fc006ff7c8a0ec84769cfd646a295
antonio_oreany <mosqua130278>
2015-08-03 22:59:19 +0300

    Finally I've fixed bug! It does work at 4 and 5-element arrays(I think, and so on...). I made another if-else statement.

---
+++ src/Recursion/Permutation3.java
@@ -0,0 +1,33 @@
+package Recursion;
+
+import java.util.Arrays;
+
+/**
+ * Created by oreany on 02.08.15.
+ */
+public class Permutation3 {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5};
+        permutation(arr, arr.length);
+    }
+
+    private static void permutation(int[] arr, int size) {
+        int j;
+        if (size < 2) {
+            System.out.println(Arrays.toString(arr));
+        } else {
+            for (int i = 0; i < size; i++) {
+                if (i==size-1) j = 0;
+                else j = i;
+                swap(arr, j, size - 1); //size, didn't swap!!!
+                permutation(arr, size - 1);
+            }
+        }
+    }
+
+    private static void swap(int[] arr, int index0, int index1) {
+        int tmp = arr[index0];
+        arr[index0] = arr[index1];
+        arr[index1] = tmp;
+    }
+}
