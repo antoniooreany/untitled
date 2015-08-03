6666f956edb4b386e50bcc410e1be8492bf8fc47
antonio_oreany <mosqua130278>
2015-08-03 22:15:02 +0300

    I've made a mistake. I didn't fix the bug! It do work only at 1, 2 and 3-element arrays!

--- src/Recursion/Permutation2.java
+++ src/Recursion/Permutation2.java
@@ -7,7 +7,7 @@ import java.util.Arrays;
  */
 public class Permutation2 {
     public static void main(String[] args) {
-        int[] arr = {1, 2, 3};
+        int[] arr = {1, 2, 3, 4};
         permutation(arr, arr.length);
     }
 
